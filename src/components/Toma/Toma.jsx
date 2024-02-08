fd1ee6d5eb94d1eb78e0d575d1e9a43c5ec50cab
Jesus Pichon <jesus.pichon@alumno.buap.mx>
2024-02-07 22:05:42 -0600

    Configuracion de iconos sobre el proyecto, agregacion de seleccion en la pantalla de tomas

--- src/components/Toma/Toma.jsx
+++ src/components/Toma/Toma.jsx
@@ -1,20 +1,71 @@
-import { Image, Card } from "@rneui/base";
-import { View, Text } from "react-native";
+import { Image} from "@rneui/base";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { principal, secundario } from "../../styles/style-colors";
+import { useState } from "react";
+
+const data = [
+  'Cientifico:',
+  'Familia:',
+  'Nombre local:',
+  'Localidad:',
+  'Municipio:',
+  'Estado:',
+  'Altitud:',
+  'Coordenadas:',
+  'Tipo de vegetacion:',
+];
 
 
 
-const Toma = (props) => {
+const Toma = ({ navigation, item, seleccionar }) => {
+
+  const [isSelected, setIsSelected] = useState(false);
+
+
+  const style_toma = StyleSheet.create({
+    view_toma: {
+      flexDirection: 'row',
+      margin: 5,
+      backgroundColor: isSelected ? '#1A6CA2' : principal,
+      borderRadius: 16,
+    },
+
+    text_card: {
+      fontSize: 12,
+      color: 'white'
+    },
+  });
+
   return (
-    <Card>
-      <View>
-        <Image
-          source={require('../../assets/images/flor.png')}
-          style={{ width: 50, height: 50, marginVertical: 30 }} />
+    <TouchableOpacity
+      onPress={() => { navigation.navigate('InformacionToma') }}
+      onLongPress={() => {
+        seleccionar(item);
+        setIsSelected(!isSelected);
+      }}>
+
+      <View style={style_toma.view_toma}>
+
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require('../../assets/images/nature.jpg')}
+            style={{ width: 150, height: 160, borderRadius: 10 }}
+          />
+        </View>
+
+        <View style={{ flex: 2, padding: 5 }}>
+          {data.map((item, index) => {
+            return (
+              <Text
+                key={index}
+                style={style_toma.text_card}>
+                {item}
+              </Text>);
+          })}
+        </View>
+
       </View>
-      <View>
-        <Text>Familia: {props.item.Familia}</Text>
-      </View>
-    </Card>
+    </TouchableOpacity>
   );
 }
 
