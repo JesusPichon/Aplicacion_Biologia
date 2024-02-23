d087ba0ee6e4b6ab16edd0f53a51f47bb04ede4f
kreork <chistian6167@gmail.com>
2024-02-22 22:53:44 -0600

    Problemas de diseño arreglado, uno diseño del modal

--- src/components/Canal/Canal.jsx
+++ src/components/Canal/Canal.jsx
@@ -1,21 +1,21 @@
 import { View, Text, TouchableOpacity, Animated, ImageBackground } from "react-native";
 import styles from "../../styles/style-app";
+import stylesCanales from "../../screens/canales/style-canales";
 
-//Revisar por que no se ve la imagen del canal 
 
 const Canal = ({ animacion, navigation, informacion }) => {
 
 
     return (
         <Animated.View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', marginBottom: 32, transform: [{ scale: animacion }] }}>
-            <View style={[styles.cardVertical, styles.fondoT, { width: '48%' }]}>
-                <View style={[styles.cardVImagen]}>
+            <View style={[stylesCanales.cardVertical, styles.fondoT, { width: '48%' }]}>
+                <View style={[stylesCanales.cardVImagen]}>
                     <ImageBackground source={require('../../assets/images/Campo_flores.jpg')}
                         resizeMode="cover"
                         style={styles.image}>
                     </ImageBackground>
                 </View>
-                <TouchableOpacity style={[styles.botongrupo, styles.fondoP]}
+                <TouchableOpacity style={[stylesCanales.botongrupo, styles.fondoP]}
                     onPress={() => { navigation.navigate('Tomas') }}>
                     <Text style={[styles.textT, { textAlign: 'center', fontWeight: 'bold' }]}>
                         {informacion.nombre}
