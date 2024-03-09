9a0f3022490fd55e3b89e2ea3125f9b5cdb525fd
Jesus Pichon <jesuspichonramirez@gmail.com>
2024-03-08 21:51:20 -0600

    agregando checkbox

--- src/components/Grupo/Grupo.jsx
+++ src/components/Grupo/Grupo.jsx
@@ -2,42 +2,54 @@ import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
 import styles from "../../styles/style-app";
 import stylesCanales from "../../screens/grupos/style-canales";
 import { useState } from "react";
-import { principal, secundario } from "../../styles/style-colors";
+import { principal } from "../../styles/style-colors";
+import { CheckBox } from "@rneui/themed";
 
 
 const Grupo = ({ navigation, informacion, nombre, deseleccionar, seleccionar }) => {
 
 
     const [isSelected, setSelected] = useState(false);
+    const [eliminar, setEliminar] = useState(false);
+
+    function handleSeleccionar() {
+        (isSelected) ? deseleccionar(informacion) : seleccionar(informacion);
+        setSelected(!isSelected);
+    }
 
     return (
-        //<Animated.View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', marginBottom: 32, transform: [{ scale: animacion }] }}>
         <TouchableOpacity
             style={[stylesCanales.cardVertical, styles.fondoT, { width: '45%', marginBottom: 20, margin: 10 }]}
             onPress={() => { navigation.navigate('Tomas', { nombre }) }}
-            onLongPress={() => {
-                if (isSelected) {
-                    deseleccionar(informacion);
-                } else {
-                    seleccionar(informacion);
-                }
-                setSelected(!isSelected);
-            }}>
+            onLongPress={() => handleSeleccionar()}>
 
             <View style={[stylesCanales.cardVImagen]}>
+
                 <ImageBackground
                     source={require('../../assets/images/nature.jpg')}
                     resizeMode="cover"
                     style={styles.image}>
                 </ImageBackground>
+
             </View>
-            <View style={[styles.botongrupo, {backgroundColor: isSelected ? 'red' : principal}]}>
+
+            <View style={[styles.botongrupo, { backgroundColor: isSelected ? 'red' : principal }]}>
                 <Text style={[styles.textT, { textAlign: 'center', fontWeight: 'bold' }]}>
                     {informacion}
                 </Text>
+
+                {() => {
+                    if (eliminar)
+                        return (
+                            <CheckBox>
+                                
+                            </CheckBox>
+                        );
+                }}
+
             </View>
+
         </TouchableOpacity>
-        //</Animated.View>
     );
 }
 
