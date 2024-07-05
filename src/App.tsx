13d7b5d7196cbad347841fa3275f73201af63eee
EnriqueRV10 <sergioenri2001@gmail.com>
2024-07-05 17:32:50 -0600

    Implementacion incial de Redux Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,30 @@ import CamposPred from "./screens/campos-predeterminados/screen-camposPred";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CacheProvider } from "./services/storage/CacheContext";
+import { Provider } from "react-redux";
+import store from "./services/redux/store";
 
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <CacheProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Inicio" component={Inicio} />
-          <Stack.Screen name="Formulario" component={Formulario} />
-          <Stack.Screen name="Tomas" component={Tomas} />
-          <Stack.Screen name="InformacionToma" component={InformacionToma} />
-          <Stack.Screen name="Grupos" component={Grupos} />
-          <Stack.Screen name="Editar" component={Editar} />
-          <Stack.Screen name="FAQ" component={FAQ} />
-          <Stack.Screen name="CamposPred" component={CamposPred}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </CacheProvider>
-    
+    // <Provider store={store}>
+      <CacheProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Inicio" component={Inicio} />
+            <Stack.Screen name="Formulario" component={Formulario} />
+            <Stack.Screen name="Tomas" component={Tomas} />
+            <Stack.Screen name="InformacionToma" component={InformacionToma} />
+            <Stack.Screen name="Grupos" component={Grupos} />
+            <Stack.Screen name="Editar" component={Editar} />
+            <Stack.Screen name="FAQ" component={FAQ} />
+            <Stack.Screen name="CamposPred" component={CamposPred}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </CacheProvider>
+    // </Provider>
   );
 }
 
