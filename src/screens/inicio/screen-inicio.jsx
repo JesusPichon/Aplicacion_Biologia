494711b4683c44791c97dde5c15a3b02b3971a38
Abnerpino <abnerpino15@gmail.com>
2024-07-24 08:47:43 -0600

    Se agrego el diseño del Login a la pantalla de Inicio

--- src/screens/inicio/screen-inicio.jsx
+++ src/screens/inicio/screen-inicio.jsx
@@ -10,6 +10,9 @@ import {
     useColorScheme,
     Appearance,
     AppState,
+    StyleSheet,
+    TextInput,
+    Image,
 } from "react-native";
 import styles from '../../styles/style-app';
 import animaciones from '../../components/animaciones/animaciones';
@@ -111,7 +114,38 @@ const Inicio = ({ navigation }) => {
     }, [modeTheme]);
 
     const theme = themes[currentTheme] || themes[systemTheme] || themes.light;
-    const { imageBackgroundInicio, logoInicio, colorStatusBarInicio } = theme;
+    const { imageBackgroundInicio, logoInicio, colorStatusBarInicio, iconoUsuario, iconoContraseña, colorPrimario, colorTerciario, colorTexto } = theme;
+
+    const styles = StyleSheet.create({
+        input: {
+          backgroundColor: colorPrimario,
+          borderColor: colorTerciario,
+          color: colorTexto,
+          fontWeight: 'bold',
+          borderWidth: 2,
+          borderRadius: 10,
+          paddingLeft: 10,
+          marginBottom: 25,
+          height: 40,
+          width: '85%'
+        },
+        button: {
+          backgroundColor: colorTerciario,
+          borderRadius: 25,
+          width: '35%',
+          paddingVertical: 10,
+          alignItems: 'center',
+        },
+        buttonText: {
+          color: '#fff',
+          fontSize: 16,
+          fontWeight: 'bold',
+        },
+        labelText: {
+          color: colorTexto,
+          fontSize: 16,
+        },
+    });
 
     return (
         <ImageBackground source={imageBackgroundInicio} resizeMode="cover" style={{ flex: 1, width: '100%', height: '100%' }}>
@@ -120,17 +154,44 @@ const Inicio = ({ navigation }) => {
                     animated={true}
                     backgroundColor={colorStatusBarInicio}
                 />
-                <Animated.View style={{ flex: 10, flexDirection: 'row', overflow: 'hidden', transform: [{ translateY: translateAnimDOWN }, { scale: unoAnim }] }}>
+                <Animated.View style={{ flex: 1, flexDirection: 'row', overflow: 'hidden', transform: [{ translateY: translateAnimDOWN }, { scale: unoAnim }] }}>
                     <View style={{ flex: 1 }}></View>
                     <ImageBackground source={logoInicio} resizeMode="contain" style={{ flex: 8 }}></ImageBackground>
                     <View style={{ flex: 1 }}></View>
                 </Animated.View>
-                <Animated.View style={{ flex: 6, justifyContent: 'center', opacity: unoAnim }}>
-                    <TouchableOpacity onPress={() => { navigation.navigate('Home'); }}>
-                        <Text style={[styles.boton, styles.fondoP, styles.textT, { paddingHorizontal: 25, paddingVertical: 15, fontSize: 18, fontWeight: 'bold' }]}>
-                            Entrar
-                        </Text>
+                <Animated.View style={{ flex: 1, width: '100%', opacity: unoAnim }}>
+                    <View style={{width: '80%',  alignSelf: 'center'}}>
+                        <View style={{flexDirection: 'row', alignSelf: 'center' }}>
+                            <Image source={iconoUsuario} style={{ width: 40, height: 40, marginRight: 10 }}/>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Nombre de Usuario"
+                                placeholderTextColor="#888"
+                            />
+                        </View>
+                        <View style={{flexDirection: 'row', alignSelf: 'center' }}>
+                            <Image source={iconoContraseña} style={{ width: 45, height: 45, marginRight: 5 }}/>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Contraseña"
+                                secureTextEntry
+                                placeholderTextColor="#888"
+                            />
+                        </View>
+                        <TouchableOpacity style={{ alignItems: 'flex-end' }}>
+                            <Text style={[styles.labelText, { marginTop: -20, textDecorationLine: 'underline' }]}>¿Olvidaste tu contraseña?</Text>
+                        </TouchableOpacity>
+                        
+                    </View>
+                    <TouchableOpacity style={[styles.button, { alignSelf: 'center', marginTop: 25 }]}  onPress={() => { navigation.navigate('Home'); }}>
+                        <Text style={styles.buttonText}>Iniciar Sesión</Text>
                     </TouchableOpacity>
+                    <View style={{ flexDirection: 'column', paddingLeft: 30,  }}>
+                        <Text style={[styles.labelText, { marginTop: 100 }]}>¿No tienes cuenta?</Text>
+                        <TouchableOpacity style={[styles.button, { marginTop: 5 }]}>
+                            <Text style={styles.buttonText}>Registrate</Text>
+                        </TouchableOpacity>
+                    </View>
                 </Animated.View>
             </View>
         </ImageBackground>
