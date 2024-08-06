3d0c1dd0af7c96241fedcf0bf6dc8cf3266b3640
irvyn2703 <tosolatoso17@gmail.com>
2024-08-05 21:38:02 -0600

    cambios a la pantalla FAQ

--- src/components/FAQ/Nosotros.jsx
+++ src/components/FAQ/Nosotros.jsx
@@ -2,6 +2,7 @@ import styles from '../../styles/style-app';
 import { principal, tercero, cuarto } from '../../styles/style-colors';
 import imagenEquipo from '../../assets/images/logoEquipo.jpg'
 import imagenBiologia from '../../assets/images/buap.png'
+import { useSelector, useDispatch } from 'react-redux';
 import React, { useState } from 'react';
 
 import {
@@ -9,83 +10,156 @@ import {
     View,
     TouchableWithoutFeedback,
     ImageBackground,
+    useColorScheme,
     Image,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    Linking
 } from "react-native";
 
 const Nosotros = () => {
-    const SobreNosotros = [
-        { titulo: 'Facultad de Ciencias de la Computación', contenido: 'Como estudiantes de la Ingeniería en Ciencias de la Computación, somos programadores apasionados por el desarrollo y la innovación tecnológica. En conjunto, creamos esta aplicación, que esperamos, sea de gran beneficio para la investigación biológica. \n\nJesús Pichón Ramírez\nContacto: jesus.pichon@alumno.buap.mx\n\nAbner Pino Federico\nContacto: abner.pino@alumno.buap.mx\n\nSergio Rivera Vidal\nContacto: sergio.riverav@alumno.buap.mx\n\nIrvyn Xicale Cabrera\nContacto: irvyn.xicale@.alumno.buap.mx\n\nChristian Santiago Rodríguez\nContacto: christian.santiagor@alumno.buap.mx'},
-        { titulo: 'Facultad de Ciencias Biológicas', contenido: 'El Dr. Carlos Alberto Ruiz Jiménez estuvo al frente de este programa de Servicio Social llamado "DESARROLLO DE APLICACIONES WEB Y MÓVILES PARA LA INVESTIGACIÓN BIOLÓGICA". \n\nContacto: carlos.ruizj@correo.buap.mx'},
-    ];
+    const systemTheme = useColorScheme();
 
-    const Preguntas = [
-        { pregunta: '¿Cuál es el tamaño de la imagen para las impresiones?', respuesta: 'Puedes utilizar cualquier imagen, pero recomendamos 755px de ancho y 60px de alto o cualquier escala de estas medidas para evitar errores visuales. También puedes optar por no elegir ninguna imagen.' },
-        { pregunta: '¿Puedo seleccionar qué campos imprimir de mi toma?', respuesta: 'Sí, solo selecciona la toma y te mandará a una nueva pantalla donde podrás seleccionar qué campos quieres imprimir y posteriormente selecciona imprimir.' },
-        { pregunta: '¿Es obligatorio elegir una imagen al momento de crear mi toma?', respuesta: 'No, es opcional elegir una imagen, pero en caso de no hacerlo, se cargará por defecto una imagen. En el caso de que la imagen seleccionada se borre del dispositivo, se cargará otra imagen alusiva al error.' },
-    ];
+    const { currentTheme, themes, modeTheme } = useSelector((state) => state.theme);
+    const theme = themes[currentTheme] || themes[systemTheme] || themes.light;
+    const { 
+        logoInicio, 
+        colorPrimario,
+        colorSecundario,
+        colorTerciario,
+        colorCuaternario,
+        colorQuinario,
+    } = theme;
 
-    const renderImagen = (titulo) => {
-        //console.log(titulo);
-        switch (titulo) {
-            case 1:
-                //console.log(imagenEquipo)
-                return (
-                    <View>
-                        <Image style={{marginBottom:10}} source={require('../../assets/images/res1.jpg')} />
-                        <Image style={{marginBottom:10}} source={require('../../assets/images/res2.jpg')} />
-                    </View>
-                );
-           
-            default:
-                return null; // Retornar null en lugar de una cadena vacía
+    const desarrolladores = [
+        {
+            nombre: 'Irvyn Xicale Cabrera',
+            github: 'irvyn2703',
+            url: 'https://irvyn2703.github.io/',
+            posicion: true
+        },
+        {
+            nombre: 'Desarrollador 2',
+            github: 'JesusPichon',
+            url: 'https://github.com/JesusPichon',
+            posicion: false
+        },
+        {
+            nombre: 'Desarrollador 3',
+            github: 'EnriqueRV10',
+            url: 'https://github.com/EnriqueRV10',
+            posicion: true
+        },
+        {
+            nombre: 'Desarrollador 4',
+            github: 'Abnerpino',
+            url: 'https://github.com/Abnerpino',
+            posicion: false
+        },
+        {
+            nombre: 'Desarrollador 5',
+            github: 'Kreork',
+            url: 'https://github.com/Kreork',
+            posicion: true
         }
-    };
+    ];
 
+    const lodoMayor = Dimensions.get('screen').width > Dimensions.get('screen').height ? Dimensions.get('screen').width : Dimensions.get('screen').height;
 
-    const [expandedIndex, setExpandedIndex] = useState(null);
+    const localStyles = StyleSheet.create({
+        imagenBackground: {
+            opacity:0.1,
+            minHeight: lodoMayor,
+            minWidth: lodoMayor,
+            position: 'absolute',
+            top: 30,
+            left: 50
+        },
+        Borde: {
+            borderColor: colorQuinario,
+            borderWidth: 3,
+        },
+        Titulo: {
+            fontSize: 24,
+            fontWeight: 'bold',
+            color: colorQuinario,
+            marginVertical: 20,
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            margin: 0,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            borderRadius: 20,
+        },
+        TextContenido: {
+            textTransform:'lowercase',
+            textAlign: 'justify',
+            fontSize: 16,
+            fontWeight: 'bold',
+            color: colorQuinario,
+            opacity: 0.6
+        },
+        DevContenedor: {
+            backgroundColor:colorQuinario, 
+            paddingVertical:15, 
+            paddingHorizontal:20,
+            borderRadius:20, 
+            zIndex:1
+        },
+        DevText: {
+            color:colorPrimario, 
+            fontSize:16, 
+            fontWeight:'bold',
+            textTransform:'capitalize'
+        }
+    });
 
     return (
-        <View style={{ backgroundColor: tercero }}>
-            <View style={{alignItems: 'center'}}>
-                <Text style={{ fontSize: 25, fontWeight: 'bold', color: principal, marginVertical: 20 }}>¿Quiénes somos?</Text>
+        <View style={{flex:1, marginTop:20}}>
+            {/*logo*/}
+            <View style={{width:'100%', alignItems:'center', position:'relative'}}>
+                <View style={[{borderRadius:200, padding:10, width:115, height:115, zIndex:2, backgroundColor:colorPrimario},localStyles.Borde]}>
+                    <Image
+                        style={{height:85, width:85}}
+                        source={logoInicio}
+                    />
+                </View>
+                <View style={{width:'100%', height:5, backgroundColor:colorQuinario, position:'absolute', bottom:57, zIndex:1}}></View>
             </View>
-            {/* Mapeo de ¿quiénes somos? */}
-            {
-                SobreNosotros.map((item, index) => (
-                    <View key={item.titulo} style={{ marginBottom: 20, backgroundColor: principal }}>
-                        <ImageBackground source={(index % 2 === 0) ? imagenEquipo : imagenBiologia} resizeMode="cover" style={styles.image}>
-                            <View style={{ marginVertical: 15, paddingHorizontal: 10, paddingVertical: 5 }}>
-                                <Text style={{ fontSize: 25, fontWeight: 'bold', color: tercero }}>{item.titulo}</Text>
-                            </View>
-                            <View style={{ marginVertical: 10, paddingHorizontal: 10, paddingVertical: 5 }}>
-                                <Text style={{ fontSize: 16, fontWeight: 'bold', color: tercero }}>{item.contenido}</Text>
-                            </View>
-                        </ImageBackground>
-                    </View>
-                ))
-            }
-            <View style={{alignItems: 'center'}}>
-                <Text style={{ fontSize: 25, fontWeight: 'bold', color: principal, marginVertical: 20 }}>Preguntas frecuentes</Text>
+            {/* Descripción */}
+            <View style={{alignItems:'center', paddingHorizontal:20}}>
+                <Text style={[localStyles.Titulo, localStyles.Borde]}>BIO COLLECTOR</Text>
+                <Text style={[localStyles.TextContenido]}>UNA APLICACIÓN MOVIL DISEÑADA PARA LA RECOLECCIÓN DE DATOS BIOLOGICOS, OBTENIDOS EN PRUEBAS DE CAMPO. LA APLICACIÓN PERMITE A LOS USUARIOS COMPARTIR LOS DATOS DE FORMA PÚBLICA DENTRO DE LA MISMA APLICACIÓN. EL OBJETIVO ES CREAR REPOSITORIOS DE INFORMACIÓN PARA INVESTIGADORES Y ALUMNOS DENTRO DEL CAMPO DE LA BIOLOGIA.</Text>
             </View>
-            {/* Mapeo de preguntas frecuentes */}
-            {Preguntas.map((pregunta, index) => (
-                <TouchableWithoutFeedback key={index} onPress={() => setExpandedIndex(expandedIndex === index ? null : index)}>
-                    <View style={{ backgroundColor: cuarto, marginVertical: 5, padding: 10 }}>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: tercero }}>{pregunta.pregunta}</Text>
-                        {expandedIndex === index && (
-                            <View style={{alignItems: 'center'}}>
-                                <Text style={{ fontSize: 15, marginTop: 10, color: tercero }}>{pregunta.respuesta}</Text>
-                                { 
-                                renderImagen(index)
-                                }
+            {/* Desarrolladores */}
+            <View style={{alignItems:'center', paddingHorizontal:20, gap:15}}>
+                <Text style={[localStyles.Titulo, localStyles.Borde]}>Desarrolladores</Text>
+                {/* Nosotros */}
+                {desarrolladores.map((desarrollador, index) => (
+                    <View key={index}>
+                        <TouchableOpacity onPress={() => Linking.openURL(desarrollador.url)}>
+                            <View style={{flexDirection:desarrollador.posicion ? 'row' : 'row-reverse', alignItems:'center', width:'100%', justifyContent:'center', paddingHorizontal:20}}>
+                                    <Image
+                                        style={[{height:85, width:85, borderRadius:200, zIndex:2}, localStyles.Borde]}
+                                        source={{uri: `https://github.com/${desarrollador.github}.png`}}
+                                    />
+                                    <View style={[{marginLeft:desarrollador.posicion ? -10 : 0, marginRight:desarrollador.posicion ? 0 : -10},localStyles.DevContenedor]}>
+                                        <Text style={[localStyles.DevText]}>{desarrollador.nombre}</Text>
+                                    </View>
                             </View>
-                        )}
+                        </TouchableOpacity>
                     </View>
-                </TouchableWithoutFeedback>
-            ))}
+                ))}
+            </View>
+            {/* Primeros pasos */}
+            <View style={{alignItems:'center', paddingHorizontal:20}}>
+                <Text style={[localStyles.Titulo, localStyles.Borde]}>Primeros pasos</Text>
+            </View>
         </View>
     );
-    
 }
+
+
 
 export default Nosotros;
--- src/screens/FAQ/screen-FAQ.jsx
+++ src/screens/FAQ/screen-FAQ.jsx
@@ -1,29 +1,59 @@
-import styles from '../../styles/style-app';
-import { principal } from '../../styles/style-colors';
+import { useSelector, useDispatch } from 'react-redux';
 import Nosotros from '../../components/FAQ/Nosotros';
 import {
     View,
     StatusBar,
     SafeAreaView,
     ScrollView,
+    useColorScheme,
+    Image,
+    StyleSheet,
+    Dimensions
 } from "react-native";
 
 
 
 const FAQ = () => {
+    const systemTheme = useColorScheme();
+
+    const { currentTheme, themes, modeTheme } = useSelector((state) => state.theme);
+    const theme = themes[currentTheme] || themes[systemTheme] || themes.light;
+    const { 
+        logoInicio, 
+        colorPrimario,
+        colorSecundario,
+        colorTerciario,
+        colorCuaternario,
+        colorQuinario,
+    } = theme;
+    
+    const lodoMayor = Dimensions.get('screen').width > Dimensions.get('screen').height ? Dimensions.get('screen').width : Dimensions.get('screen').height;
+    const localStyles = StyleSheet.create({
+        imagenBackground: {
+            opacity:0.1,
+            minHeight: lodoMayor,
+            minWidth: lodoMayor,
+            position: 'absolute',
+            top: 30,
+            left: 50
+        },
+    });
+
     return (
-        <View style={[
-            styles.fondoT,
+        <View style={
             {
                 flex: 1,
-                /*marginTop: Constants.statusBarHeight,*/
-            }]}>
+                backgroundColor: colorPrimario
+            }}>
             <StatusBar
-                barStyle="ligth-content"
+                barStyle={currentTheme === 'light' ? 'dark-content' : 'light-content'}
                 animated={true}
-                backgroundColor={principal}
+                backgroundColor={colorPrimario}
             />
-
+            <Image
+                style={localStyles.imagenBackground}
+                source={logoInicio}
+            />
 
             <SafeAreaView>
                 <ScrollView>
