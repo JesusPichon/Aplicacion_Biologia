2d61121244c4844bf174eaa6fea981945768880f
EnriqueRV10 <sergioenri2001@gmail.com>
2024-08-13 07:38:06 -0600

    Se creó un stack de pantallas para categoria Explorar

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,3 @@
-//Navegacion Original
-
-// import React, { useEffect } from "react";
-// import Prueba from "./screens/prueba";
-// import Inicio from "./screens/inicio";
-// import Formulario from "./screens/formulario";
-// import InformacionToma from "./screens/info";
-// import Grupos from "./screens/grupos";
-// import Tomas from "./screens/tomas";
-// import Editar from "./screens/editar";
-// import FAQ from "./screens/FAQ/screen-FAQ";
-// import CamposPred from "./screens/campos-predeterminados/screen-camposPred";
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { CacheProvider } from "./services/storage/CacheContext";
-// import { Provider } from "react-redux";
-// import store from "./services/redux/store";
-
-
-// const Stack = createNativeStackNavigator();
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <CacheProvider>
-//         <NavigationContainer>
-//           <Stack.Navigator screenOptions={{ headerShown: false }}>
-//             {/* <Stack.Screen name="Prueba" component={Prueba} /> */}
-//             <Stack.Screen name="Inicio" component={Inicio} />
-//             <Stack.Screen name="Formulario" component={Formulario} />
-//             <Stack.Screen name="Tomas" component={Tomas} />
-//             <Stack.Screen name="InformacionToma" component={InformacionToma} />
-//             <Stack.Screen name="Grupos" component={Grupos} />
-//             <Stack.Screen name="Editar" component={Editar} />
-//             <Stack.Screen name="FAQ" component={FAQ} />
-//             <Stack.Screen name="CamposPred" component={CamposPred}/>
-//           </Stack.Navigator>
-//         </NavigationContainer>
-//       </CacheProvider>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
-// Implementacion de Navegacion con componente Drawer
-
 import React from 'react';
 
 import Inicio from './screens/inicio';
@@ -65,10 +18,6 @@ import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@rea
 import { CacheProvider } from './services/storage/CacheContext';
 import { Provider } from 'react-redux';
 import store from './services/redux/store';
-import { View, Text, Animated, FlatList } from "react-native";
-import { useSelector } from 'react-redux';
-import { Icon } from 'react-native-elements';
-import { cuartoFePro, principal, principalFePro, quintoFePro, secundarioFePro, terceroFePro } from "../src/styles/style-colors";
 import   CustomDrawer  from './components/Drawer/customDrawer';
 
 const Stack = createNativeStackNavigator();
@@ -83,7 +32,13 @@ const MisGruposStack = () => (
     <Stack.Screen name="Editar" component={Editar} />
     <Stack.Screen name="CamposPred" component={CamposPred}/>
     <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="Explorar" component={Explorar} />
+  </Stack.Navigator>
+);
+
+const ExplorarStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Drawer.Screen name="ExplorarScreen" component={Explorar} />
+    {/* Aqui van el resto de pantallas para ver contenido del servidor */}
   </Stack.Navigator>
 );
 
@@ -95,12 +50,12 @@ const HomeDrawer = () => (
     drawerContent={(props) => <CustomDrawer {...props} />}
   >
     <Drawer.Screen name="MisGrupos" component={MisGruposStack} />
-    {/* <Drawer.Screen name="Explorar" component={ExplorarStack} /> */}
+    <Drawer.Screen name="Explorar" component={ExplorarStack} />
     <Drawer.Screen name="Configuracion" component={Ajustes} />
     <Drawer.Screen name="FAQ" component={FAQ} />
-    <Drawer.Screen name="Explorar" component={Explorar} />
   </Drawer.Navigator>
 );
+
 const App = () => {
   return (
     <Provider store={store}>
