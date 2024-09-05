9c0b38184bed8f5731fe14604d39ae106c03b418
irvyn2703 <tosolatoso17@gmail.com>
2024-09-04 23:11:44 -0600

    fix(APP): eliminando tomas de pocketBase

--- src/components/Grupo/Grupo.jsx
+++ src/components/Grupo/Grupo.jsx
@@ -1,34 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, ImageBackground, FlatList } from "react-native";
-import stylesCanales from "../../screens/grupos/style-canales";
-import { useSelector } from "react-redux";
-import { Chip } from "@rneui/themed";
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  FlatList,
+} from 'react-native';
+import stylesCanales from '../../screens/grupos/style-canales';
+import {useSelector} from 'react-redux';
+import {Chip} from '@rneui/themed';
 import Snackbar from 'react-native-snackbar';
-import PocketController from "../../services/controllers/pocketController";
-import { jsonToCSV } from 'react-native-csv';
-import { getRawData, formatData, guardarArchivoCSV, columnasComillas } from "../../services/functions/export-csv";
-import GrupoController from "../../services/controllers/grupoController";
+import PocketController from '../../services/controllers/pocketController';
+import {jsonToCSV} from 'react-native-csv';
+import {
+  getRawData,
+  formatData,
+  guardarArchivoCSV,
+  columnasComillas,
+} from '../../services/functions/export-csv';
+import GrupoController from '../../services/controllers/grupoController';
 
-
-const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, selectionMode, explorar=false, misGrupos=false, item, onEliminarGrupo}) => {
-  const { currentTheme, themes } = useSelector((state) => state.theme);
+const Grupo = ({
+  navigation,
+  nombre,
+  seleccionar,
+  deseleccionar,
+  showCheckBox,
+  selectionMode,
+  explorar = false,
+  misGrupos = false,
+  item,
+  onEliminarGrupo,
+}) => {
+  const {currentTheme, themes} = useSelector(state => state.theme);
   const theme = themes[currentTheme] || themes.light;
-  const { 
-    colorPrimario,
-    colorSecundario,
-    colorTerciario,
-    colorQuinario,
-  } = theme;
+  const {colorPrimario, colorSecundario, colorTerciario, colorQuinario} = theme;
 
-  const { flagUpdate } = useSelector((state) => state.update);
+  const {flagUpdate} = useSelector(state => state.update);
 
   const [checked, setChecked] = useState(false);
   const [showExportOptions, setShowExportOptions] = useState(false);
   const [totalTomas, setTotalTomas] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  const { user, isAuthenticated } = useSelector(state => state.auth);
-
+  const {user, isAuthenticated} = useSelector(state => state.auth);
 
   useEffect(() => {
     setChecked(false);
@@ -36,18 +51,19 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
 
   const controllerPocket = new PocketController(); //agregar controller
   const controller = new GrupoController();
-  
+
   useEffect(() => {
     if (!explorar) {
       // Obtén el total de tomas para este grupo cuando se monte el componente
-      controller.obtenerNumeroTomas(nombre)
+      controller
+        .obtenerNumeroTomas(nombre)
         .then(total => {
           setTotalTomas(total);
         })
         .catch(error => {
-          console.error("Error al obtener el total de tomas:", error);
+          console.error('Error al obtener el total de tomas:', error);
         });
-    }else{
+    } else {
       setTotalTomas(item.numero_tomas);
     }
   }, [flagUpdate]);
@@ -60,56 +76,56 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
     setShowExportOptions(false);
     try {
       const datosConsulta = await getRawData(nombre);
-      
+
       const datosFormateados = await formatData(datosConsulta);
-      
-      const csv = jsonToCSV(datosFormateados, { quotes: columnasComillas });
+
+      const csv = jsonToCSV(datosFormateados, {quotes: columnasComillas});
       await guardarArchivoCSV(nombre, csv)
-        .then((mensaje) => {
+        .then(mensaje => {
           Snackbar.show({
             text: mensaje,
-            duration: Snackbar.LENGTH_SHORT
+            duration: Snackbar.LENGTH_SHORT,
           });
         })
-        .catch((error) => {
+        .catch(error => {
           Snackbar.show({
             text: error,
-            duration: Snackbar.LENGTH_SHORT
+            duration: Snackbar.LENGTH_SHORT,
           });
         });
     } catch (error) {
-      console.error("Error al exportar: ", error);
+      console.error('Error al exportar: ', error);
       Snackbar.show({
-        text: "Error al exportar: " + error.message,
-        duration: Snackbar.LENGTH_SHORT
+        text: 'Error al exportar: ' + error.message,
+        duration: Snackbar.LENGTH_SHORT,
       });
     }
   };
 
   const handlePublicar = async () => {
-    try{
+    try {
       const datosConsulta = await getRawData(nombre);
       const datosFormateados = await formatData(datosConsulta);
 
       const dataGrupo = {
         autor: user,
         nombre: nombre,
-        numero_tomas: totalTomas
-      }
+        numero_tomas: totalTomas,
+      };
 
       await controllerPocket.publicarGrupo(dataGrupo, datosFormateados);
 
       Snackbar.show({
-      text: "Grupo Publicado",
-      duration: Snackbar.LENGTH_SHORT
+        text: 'Grupo Publicado',
+        duration: Snackbar.LENGTH_SHORT,
       });
     } catch (error) {
       Snackbar.show({
-        text: "Error al publicar el grupo",
-        duration: Snackbar.LENGTH_SHORT
+        text: 'Error al publicar el grupo',
+        duration: Snackbar.LENGTH_SHORT,
       });
-      console.error("Error al publicar el grupo:", error);
-      throw new Error("Error al publicar el grupo: " + error.message);
+      console.error('Error al publicar el grupo:', error);
+      throw new Error('Error al publicar el grupo: ' + error.message);
     }
     setShowExportOptions(false);
   };
@@ -123,60 +139,79 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
       }
       setChecked(!checked);
     } else {
-      explorar ? id = item.id : null;
-      explorar ? navigation.navigate('TomasExplorar', {id, nombre}) : navigation.navigate('Tomas', {nombre});
+      explorar ? (id = item.id) : null;
+      explorar
+        ? navigation.navigate('TomasExplorar', {id, nombre})
+        : navigation.navigate('Tomas', {nombre});
     }
   };
 
-  const exportLocal = [{ id: '1', label: 'Descargar en este dispositivo', action: handleLocalExport }];
+  const exportLocal = [
+    {
+      id: '1',
+      label: 'Descargar en este dispositivo',
+      action: handleLocalExport,
+    },
+  ];
   const exportLocalPb = [
-    { id: '1', label: 'Descargar en este dispositivo', action: handleLocalExport },
-    { id: '2', label: 'Compartir públicamente', action: handlePublicar }
+    {
+      id: '1',
+      label: 'Descargar en este dispositivo',
+      action: handleLocalExport,
+    },
+    {id: '2', label: 'Compartir públicamente', action: handlePublicar},
   ];
 
   const renderExportOptions = () => (
-    <View style={[stylesCanales.exportOptionsContainer, { backgroundColor: colorPrimario }]}>
-    <FlatList
-      data={!isAuthenticated ? exportLocal : exportLocalPb}
-      renderItem={({ item }) => (
-        <TouchableOpacity 
-          style={[stylesCanales.exportOptionButton, { backgroundColor: colorTerciario }]} 
-          onPress={item.action}
-        >
-          <Text style={{  color: "white"}}>{item.label}</Text>
-        </TouchableOpacity>
-      )}
-      keyExtractor={item => item.id}
-    />
-  </View>
-
+    <View
+      style={[
+        stylesCanales.exportOptionsContainer,
+        {backgroundColor: colorPrimario},
+      ]}>
+      <FlatList
+        data={!isAuthenticated ? exportLocal : exportLocalPb}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={[
+              stylesCanales.exportOptionButton,
+              {backgroundColor: colorTerciario},
+            ]}
+            onPress={item.action}>
+            <Text style={{color: 'white'}}>{item.label}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+      />
+    </View>
   );
 
-  const imageSource = explorar && item.url_imagen
-    ? { uri: item.url_imagen }  // Si `url_imagen` es una URL remota, se usa `uri`
-    : require('../../assets/images/nature.jpg');
+  const imageSource =
+    explorar && item.url_imagen
+      ? {uri: item.url_imagen} // Si `url_imagen` es una URL remota, se usa `uri`
+      : require('../../assets/images/nature.jpg');
 
   function lanzarAlerta(mensaje) {
     setTimeout(() => {
-        Snackbar.show({
-            text: mensaje,
-            duration: Snackbar.LENGTH_SHORT
-        });
+      Snackbar.show({
+        text: mensaje,
+        duration: Snackbar.LENGTH_SHORT,
+      });
     }, 200);
   }
 
-  const handleEliminarGrupo = async (grupoId) => {
+  const handleEliminarGrupo = async grupoId => {
     try {
-        lanzarAlerta("Eliminando el grupo: " + item.nombre);
-        await controllerPocket.EliminarGrupo(grupoId);
-        onEliminarGrupo(grupoId); // Llamar la función que actualiza el estado en el componente padre
-        lanzarAlerta("Grupo eliminado exitosamente");
+      lanzarAlerta('Eliminando el grupo: ' + item.nombre);
+      await controllerPocket.ObtenerTomasFull(item.id);
+      await controllerPocket.EliminarGrupo(grupoId);
+      onEliminarGrupo(grupoId); // Llamar la función que actualiza el estado en el componente padre
+      lanzarAlerta('Grupo eliminado exitosamente');
     } catch (error) {
-        lanzarAlerta("Error al eliminar el grupo");
+      lanzarAlerta('Error al eliminar el grupo');
     }
   };
 
-  const formatearFecha = (fecha) => {
+  const formatearFecha = fecha => {
     const date = new Date(fecha);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -185,24 +220,38 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
   };
 
   return (
-
-    <TouchableOpacity style={stylesCanales.cardVertical} onPress={handleSeleccionarGrupo} onLongPress={selectionMode}>
+    <TouchableOpacity
+      style={stylesCanales.cardVertical}
+      onPress={handleSeleccionarGrupo}
+      onLongPress={selectionMode}>
       <ImageBackground
-        source={isLoading ? require('../../assets/images/nature.jpg') : imageSource}
+        source={
+          isLoading ? require('../../assets/images/nature.jpg') : imageSource
+        }
         resizeMode="cover"
         style={stylesCanales.image}
         onLoadEnd={() => setIsLoading(false)}
       />
-      <View style={{ flex: 1, flexDirection: 'row', width: '100%' }}>
-        <View style={[stylesCanales.nombreView, { backgroundColor: colorPrimario }]}>
-          <Text style={[stylesCanales.nombreViewText, { color: colorQuinario}]}>
+      <View style={{flex: 1, flexDirection: 'row', width: '100%'}}>
+        <View
+          style={[stylesCanales.nombreView, {backgroundColor: colorPrimario}]}>
+          <Text style={[stylesCanales.nombreViewText, {color: colorQuinario}]}>
             {explorar ? (
-              <View style={{flexDirection:"column"}}>
-                <Text style={{ color: colorQuinario, fontWeight: 'bold' }}>{nombre}</Text>
-                <Text style={{ color: colorQuinario}}><Text style={{fontWeight:'bold'}}>Publicado:</Text> {formatearFecha(item.created)}</Text>
-                <Text style={{ color: colorQuinario}}><Text style={{fontWeight:'bold'}}>Autor:</Text> {item.autor}</Text>
+              <View style={{flexDirection: 'column'}}>
+                <Text style={{color: colorQuinario, fontWeight: 'bold'}}>
+                  {nombre}
+                </Text>
+                <Text style={{color: colorQuinario}}>
+                  <Text style={{fontWeight: 'bold'}}>Publicado:</Text>{' '}
+                  {formatearFecha(item.created)}
+                </Text>
+                <Text style={{color: colorQuinario}}>
+                  <Text style={{fontWeight: 'bold'}}>Autor:</Text> {item.autor}
+                </Text>
               </View>
-            ) : nombre}
+            ) : (
+              nombre
+            )}
           </Text>
           {showCheckBox ? (
             <Chip
@@ -213,7 +262,7 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
                 color: 'white',
               }}
               onPress={handleSeleccionarGrupo}
-              buttonStyle={{ backgroundColor: 'red' }}
+              buttonStyle={{backgroundColor: 'red'}}
             />
           ) : explorar ? (
             misGrupos ? (
@@ -225,7 +274,7 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
                     size: 25,
                     color: 'white',
                   }}
-                  buttonStyle={{ backgroundColor: 'red' }}
+                  buttonStyle={{backgroundColor: 'red'}}
                   onPress={() => handleEliminarGrupo(item.id)}
                 />
               </>
@@ -241,18 +290,20 @@ const Grupo = ({ navigation, nombre, seleccionar, deseleccionar, showCheckBox, s
                 color: 'white',
               }}
               onPress={handleExportOptions}
-              buttonStyle={{ backgroundColor: colorTerciario }}
+              buttonStyle={{backgroundColor: colorTerciario}}
             />
           )}
         </View>
-        <View style={[stylesCanales.tomasView, { backgroundColor: colorTerciario }]}>
-          <Text style={[stylesCanales.tomasViewText, { color: "white"}]}>Tomas: {totalTomas}</Text>
+        <View
+          style={[stylesCanales.tomasView, {backgroundColor: colorTerciario}]}>
+          <Text style={[stylesCanales.tomasViewText, {color: 'white'}]}>
+            Tomas: {totalTomas}
+          </Text>
         </View>
       </View>
       {showExportOptions && renderExportOptions()}
     </TouchableOpacity>
   );
-}
+};
 
 export default Grupo;
-
--- src/services/controllers/pocketController.js
+++ src/services/controllers/pocketController.js
@@ -1,95 +1,110 @@
 import pb from '../PocketBase/pocketbase';
 
 class PocketController {
-
-    async obtenerGruposDeOtros(pagina, itemsPorPagina, buscar) {
-        try {
-            const user = pb.authStore.model.username;
-            const filter = `autor != "${user}"${buscar && buscar != "" ? ` && nombre ~ "${buscar}"` : ''}`;
-            const records = await pb.collection('grupos').getList(pagina, itemsPorPagina, {
-                sort: '-created',
-                filter: filter,
-            });
-            return records;
-        } catch (error) {
-            console.error("Error al obtener grupos:", error);
-            throw new Error("Error al obtener grupos: " + error.message);
-        }
+  async obtenerGruposDeOtros(pagina, itemsPorPagina, buscar) {
+    try {
+      const user = pb.authStore.model.username;
+      const filter = `autor != "${user}"${
+        buscar && buscar != '' ? ` && nombre ~ "${buscar}"` : ''
+      }`;
+      const records = await pb
+        .collection('grupos')
+        .getList(pagina, itemsPorPagina, {
+          sort: '-created',
+          filter: filter,
+        });
+      return records;
+    } catch (error) {
+      console.error('Error al obtener grupos:', error);
+      throw new Error('Error al obtener grupos: ' + error.message);
     }
+  }
 
-    async obtenerMisGrupos(pagina, itemsPorPagina, buscar) {
-        try {
-            const user = pb.authStore.model.username;
-            const filter = `autor = "${user}"${buscar ? ` && nombre ~ "${buscar}"` : ''}`;
-            const records = await pb.collection('grupos').getList(pagina, itemsPorPagina, {
-                sort: '-created',
-                filter: filter,
-            });
-            return records;
-        } catch (error) {
-            console.error("Error al obtener grupos:", error);
-            throw new Error("Error al obtener grupos: " + error.message);
-        }
+  async obtenerMisGrupos(pagina, itemsPorPagina, buscar) {
+    try {
+      const user = pb.authStore.model.username;
+      const filter = `autor = "${user}"${
+        buscar ? ` && nombre ~ "${buscar}"` : ''
+      }`;
+      const records = await pb
+        .collection('grupos')
+        .getList(pagina, itemsPorPagina, {
+          sort: '-created',
+          filter: filter,
+        });
+      return records;
+    } catch (error) {
+      console.error('Error al obtener grupos:', error);
+      throw new Error('Error al obtener grupos: ' + error.message);
     }
-    
-    async EliminarGrupo(id) {
-        try {
-            const records = await pb.collection('grupos').delete(id);
-            return records;
-        } catch (error) {
-            console.error("Error al obtener grupos:", error);
-            throw new Error("Error al obtener grupos: " + error.message);
-        }
+  }
+
+  async EliminarGrupo(id) {
+    try {
+      const records = await pb.collection('grupos').delete(id);
+      return records;
+    } catch (error) {
+      console.error('Error al obtener grupos:', error);
+      throw new Error('Error al obtener grupos: ' + error.message);
     }
+  }
 
-    // Tomas
+  // Tomas
 
-    async ObtenerTomas(id, pagina, itemsPorPagina, buscar, campo) {
-        try {
-            const filter = `id_grupo = "${id}" ${buscar ? ` && ${campo} ~ "${buscar}"` : ''}`;
-            const resultList = await pb.collection('tomas').getList(pagina, itemsPorPagina, {
-                sort: '-created',
-                filter: filter,
-            });
-            return resultList;
-        } catch (error) {
-            console.error("Error fetching list:", error);
-            return null; // O cualquier valor que tenga sentido devolver en caso de error
-        }
+  async ObtenerTomas(id, pagina, itemsPorPagina, buscar, campo) {
+    try {
+      const filter = `id_grupo = "${id}" ${
+        buscar ? ` && ${campo} ~ "${buscar}"` : ''
+      }`;
+      const resultList = await pb
+        .collection('tomas')
+        .getList(pagina, itemsPorPagina, {
+          sort: '-created',
+          filter: filter,
+        });
+      return resultList;
+    } catch (error) {
+      console.error('Error fetching list:', error);
+      return null; // O cualquier valor que tenga sentido devolver en caso de error
     }
+  }
 
-    async ObtenerTomasFull(id){
-        try {
-            const records = await pb.collection('tomas').getFullList({
-                sort: '-created',
-                filter: `id_grupo = "${id}"`,
-            });
-            return records
-        } catch (error) {
-            console.error("Error fetching list:", error);
-            return null; // O cualquier valor que tenga sentido devolver en caso de error
-        }
+  async ObtenerTomasFull(id) {
+    try {
+      // Obtener los registros del grupo especificado
+      const records = await pb.collection('tomas').getFullList({
+        sort: '-created',
+        filter: `id_grupo = "${id}"`,
+      });
+
+      // Recorrer los registros y eliminarlos
+      for (const record of records) {
+        await pb.collection('tomas').delete(record.id);
+      }
+
+      return records;
+    } catch (error) {
+      console.error('Error fetching or deleting records:', error);
+      return null; // O cualquier valor que tenga sentido devolver en caso de error
     }
-    
+  }
 
-    async publicarGrupo(dataGrupo, dataTomas) {
-        try {
-            const nuevoGrupo = await pb.collection('grupos').create(dataGrupo);
+  async publicarGrupo(dataGrupo, dataTomas) {
+    try {
+      const nuevoGrupo = await pb.collection('grupos').create(dataGrupo);
 
-            // Subir cada toma asociada a ese grupo
-            for (const toma of dataTomas) {
-                await pb.collection('tomas').create({ 
-                    ...toma,
-                    id_grupo: nuevoGrupo.id,
-                });
-            }
-
-        }catch(error){
-            console.error("Error al publicar el grupo:", error);
-            throw new Error("Error al publicar el grupo: " + error.message);
-        }
+      // Subir cada toma asociada a ese grupo
+      for (const toma of dataTomas) {
+        await pb.collection('tomas').create({
+          ...toma,
+          id_grupo: nuevoGrupo.id,
+        });
+      }
+    } catch (error) {
+      console.error('Error al publicar el grupo:', error);
+      throw new Error('Error al publicar el grupo: ' + error.message);
     }
+  }
 }
-
 
 export default PocketController;
